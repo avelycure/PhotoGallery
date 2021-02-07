da47f9284c5e2bb962da572faecba4aa9132e2fb
ivan^C <van.emelin2012@yandex.ru>
2021-02-07 18:05:00 +0300

    #PhotoGallery version 7.1. Solved some problems with keyboard in main activity.
    Signed-off-by: ivan^C <van.emelin2012@yandex.ru>

--- app/src/main/java/com/avelycure/photogallery/PhotoGallery.java
+++ app/src/main/java/com/avelycure/photogallery/PhotoGallery.java
@@ -12,6 +12,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageButton;
